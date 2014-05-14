dbc061ea544f8f0c2ac6c71a0d7e7caeec921a56
John DeSilva <desilvjo@umich.edu>
2014-05-14 03:16:32 +0200

    Ensure randomness

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :post do
-    title Faker::Lorem.words.join(' ')
-    body Faker::Lorem.paragraph(2)
-    date Kernel.rand(2).years.ago
+    title { Faker::Lorem.words.join(' ') }
+    body { Faker::Lorem.paragraph(2) }
+    date { Kernel.rand(2.years).seconds.ago }
   end
 end
