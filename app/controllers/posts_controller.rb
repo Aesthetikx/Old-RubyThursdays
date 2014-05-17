e55a27c70148ec42d6a7c15294250189810d999b
John DeSilva <desilvjo@umich.edu>
2014-05-18 00:51:05 +0200

    Infinite scrolling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.paginate(page: params[:page],
-                           per_page: params[:per_page]).order('date DESC')
+                           per_page: params[:per_page]
+                          ).order('date DESC')
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
