72d05c85c93124acf8b2eb580fee2464b55f5dd9
John DeSilva <desilvjo@umich.edu>
2014-05-27 20:57:55 +0200

    Clean sidebar, add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+require 'digest/md5'
+
 module ApplicationHelper
+  def gravatar_url email
+    hash = gravatar_hash email
+    "http://www.gravatar.com/avatar/#{hash}?s=48"
+  end
+
+  private
+
+  def gravatar_hash email
+    Digest::MD5.hexdigest(email.downcase)
+  end
 end
